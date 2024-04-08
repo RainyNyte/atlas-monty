802ebc89e31cc28f5c53c1097bcbdf978fea0880
Rain Nyte <rain_nyte@proton.me>
2024-04-08 16:05:24 -0500

    fixe missing semicollon on line 41 of monty.h

--- monty.h
+++ monty.h
@@ -38,7 +38,7 @@ typedef struct instruction_s
 } instruction_t;
 
 void clear_stack(stack_t *stack);
-void get_instruction(stack_t **stack, char arg_str, unsigned int line_number)
+void get_instruction(stack_t **stack, char arg_str, unsigned int line_number);
 void push(stack_t **stack, char arg_str, unsigned int line_number);
 void pall(stack_t **stack, char arg_str, unsigned int line_number);
 void pint(stack_t **stack, char arg_str, unsigned int line_number);
