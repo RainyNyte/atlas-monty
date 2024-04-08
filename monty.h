28c82b65b96baacead13cb00d4acd17712ee5f87
Rain Nyte <rain_nyte@proton.me>
2024-04-08 13:51:22 -0500

    changed variables used by push

--- monty.h
+++ monty.h
@@ -35,7 +35,7 @@ typedef struct instruction_s
 } instruction_t;
 
 void clear_stack(stack_t *stack);
-void push(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, int arg);
 void pall(stack_t **stack);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
